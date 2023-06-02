9585975a971f15e9c97a25d71a4ccd11e4323c3d
de4life <ageev_artur@ukr.net>
2023-06-02 18:58:31 +0300

    add extensions

---
+++ WeatherTask/Manager/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherTask
+//
+//  Created by Артур Агеев on 02.06.2023.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
