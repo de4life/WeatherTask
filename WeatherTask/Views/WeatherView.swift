f58bc2136147fcca510d42c5108a7030c773cfe7
de4life <ageev_artur@ukr.net>
2023-06-02 18:57:03 +0300

    add WeatherView

--- WeatherTask/Views/WeatherView.swift
+++ WeatherTask/Views/WeatherView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct WeatherView: View {
+    var weather: ResponseBody
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    Text("Today, \(Date().formatted(.dateTime .month().day().hour().minute()))")
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            Text(weather.weather[0].main)
+                        }
+                    }
+                    
+                }
+                .frame(width: 150, alignment: .leading)
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherView()
+        WeatherView(weather: previewWeather)
     }
 }
